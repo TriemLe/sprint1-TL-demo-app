3e08dfc44c0a1d1b724fd91ce8329f0506efcca4
EC2 Default User <ec2-user@ip-10-18-71-253.eu-west-1.compute.internal>
2026-08-25 13:26:37 +0000

    trying to do this in commandline

--- starter/src/main/java/com/neueda/leap/Main.java
+++ starter/src/main/java/com/neueda/leap/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world from the team's Sprint 1 project skeleton");
 	System.out.println("Hello from Triem's change");
+	System.out.println("Doing this on the dang terminal");
     }
 }
